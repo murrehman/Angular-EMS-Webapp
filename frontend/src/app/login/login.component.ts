38c5e74568999add3523dd0efca0ac45dbce2e55
murrehman <muteeurrehmanbajwa@gmail.com>
2023-09-01 18:20:46 +0200

    fixed minor bugs

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../appServices/auth.service';
 import { Router } from '@angular/router';
+import { SocialAuthService } from 'angularx-social-login';
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   registerUserData: any = {}
 
-  constructor(private _auth: AuthService, private router: Router) { }
+  constructor(private _auth: AuthService, private router: Router, private auth: SocialAuthService) { }
 
   ngOnInit(): void {
   }
@@ -26,6 +28,20 @@ export class LoginComponent implements OnInit {
       err => console.log(err)
     )
     // this.router.navigate(['/employee'])
+  }
+
+  googleSignup() {
+    this.auth.signIn(GoogleLoginProvider.PROVIDER_ID).then((data) => {
+      console.log(data);
+
+      this._auth.googleLogin(data).subscribe(res => {
+        console.log(res.token);
+
+        localStorage.setItem('token', res.token)
+        this.router.navigate(['/employee'])
+      })
+
+    })
   }
 
 }
